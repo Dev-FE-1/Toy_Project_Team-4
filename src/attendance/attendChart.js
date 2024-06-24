308670a7707f853baff43e16d1503eddc1df2650
wonjichoe <wonjichoe@gmail.com>
2024-06-24 14:43:18 +0900

    모달 스타일 수정 완료

--- src/attendance/attendChart.js
+++ src/attendance/attendChart.js
@@ -7,7 +7,9 @@ const currentYear = currentDate.getFullYear(); // 현재 연도
 const totalDays = new Date(currentYear, currentMonth + 1, 0).getDate(); // 현재 월의 총 일수
 
 export function createAttendanceChart(student) {
-  const ctx = document.getElementById('attendanceCanvas').getContext('2d');
+  // const ctx = document.getElementById('attendanceCanvas').getContext('2d');
+  const canvas = document.getElementById('attendanceCanvas');
+  const ctx = canvas.getContext('2d');
   
   // 출석 데이터 계산 및 텍스트로 표시할 값 설정
   const attendancePercentage = Math.round((parseInt(student.attendanceDays) / totalDays) * 100);
@@ -39,6 +41,11 @@ export function createAttendanceChart(student) {
       }
     }
   };
+
+  // 기존 차트를 파괴 (필요한 경우)
+  if (Chart.getChart(canvas)) {
+    Chart.getChart(canvas).destroy();
+  }
 
   new Chart(ctx, {
     type: 'doughnut',

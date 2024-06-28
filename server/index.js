08d185fe21d0beacd63d7fecef20de9bd5a9a339
hyojeong <okm0227@naver.com>
2024-06-28 18:24:07 +0900

    갤러리 json으로 불러오기

--- server/index.js
+++ server/index.js
@@ -110,6 +110,31 @@ app.get("/api/attendance.json", (req, res) => {
   })
 })
 
+app.get("/api/gallery.json", (req, res) => {
+  fs.readFile("./server/data/gallery.json", "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading JSON file:", err)
+      return res.status(500).send({
+        status: "Internal Server Error",
+        message: err,
+        data: null,
+      })
+    }
+
+    try {
+      const json = JSON.parse(data)
+      res.json(json)
+    } catch (parseErr) {
+      console.error("Error parsing JSON file:", parseErr)
+      return res.status(500).send({
+        status: "Internal Server Error",
+        message: parseErr,
+        data: null,
+      })
+    }
+  })
+})
+
 // app.get("/api/userInfo", (req, res) => {
 //   const sql = "SELECT * FROM userInfo"
 
